b08c532b3fe65ac20a2aaeb65fca6943587a9fd6
Aladdin Ali <15963333+NaxeCode@users.noreply.github.com>
2024-03-22 10:36:52 -0400

    changed domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://naxecode.github.io/',
-	base: '/',
 	integrations: [mdx(), sitemap()],
 });
