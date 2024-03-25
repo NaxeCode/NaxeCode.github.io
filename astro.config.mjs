869243d5f4f5f5b8350674d5787adc6ade2f8ba5
Siraj Ali <15963333+NaxeCode@users.noreply.github.com>
2024-03-24 22:12:09 -0400

    changed site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://naxecode.github.io/',
+	site: 'https://naxe.dev/',
 	integrations: [mdx(), sitemap()],
 });
