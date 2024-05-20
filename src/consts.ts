22fd5d7b2f7b8746c89d45ab61fe9a7a8fd636a6
Aladdin Ali <15963333+NaxeCode@users.noreply.github.com>
2024-05-20 19:42:54 -0400

    updated site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Naxe's Development Blog";
-export const SITE_DESCRIPTION = 'Welcome to my site!';
+export const SITE_DESCRIPTION = 'The Underbelly!';
