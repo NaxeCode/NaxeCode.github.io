10eb9f51d7cddada9fac57f648f85e9541dcecd2
Aladdin Ali <15963333+NaxeCode@users.noreply.github.com>
2024-05-23 14:23:41 -0400

    Revert "added web vitals monitoring"
    This reverts commit 6c77d7f442d1d92f26e6ef14b3b25b0bbec5b4b4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import preact from '@astrojs/preact';
-import webVitals from '@astrojs/web-vitals';
-import db from '@astrojs/db';
 
-import node from "@astrojs/node";
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  site: 'https://naxe.dev/',
-  integrations: [mdx(), sitemap(), preact({
-    devtools: true
-  }), webVitals(), db()],
-  adapter: node({
-    mode: "standalone"
-  })
+	site: 'https://naxe.dev/',
+	integrations: [mdx(), sitemap(), preact({ devtools: true })],
 });
