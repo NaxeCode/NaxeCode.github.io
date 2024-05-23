118d3777e7cec6a106cdb24d1db7e2d1450b717f
Aladdin Ali <15963333+NaxeCode@users.noreply.github.com>
2024-05-23 10:11:52 -0400

    Added database to proj

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import preact from '@astrojs/preact';
-
 import webVitals from "@astrojs/web-vitals";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://naxe.dev/',
   integrations: [mdx(), sitemap(), preact({
     devtools: true
-  }), webVitals()]
+  }), webVitals(), db()]
 });
