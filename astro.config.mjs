a7ab91514478a73845fb30db38c58f5cde6f72f5
Aladdin Ali <15963333+NaxeCode@users.noreply.github.com>
2024-05-23 14:16:04 -0400

    reverted commits

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import preact from '@astrojs/preact';
+import webVitals from '@astrojs/web-vitals';
+import db from '@astrojs/db';
 
-import webVitals from "@astrojs/web-vitals";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
   site: 'https://naxe.dev/',
   integrations: [mdx(), sitemap(), preact({
     devtools: true
-  }), webVitals()]
+  }), webVitals(), db()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
