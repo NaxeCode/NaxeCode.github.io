f8aa0718e450697cdc20f1277f431aeef5f06c20
Aladdin Ali <15963333+NaxeCode@users.noreply.github.com>
2024-06-16 22:32:45 -0400

    Overhauled site look & feel.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Naxe's Development Blog";
+export const SITE_TITLE = 'NaxeCode';
 export const SITE_DESCRIPTION = 'The Underbelly!';
+export const NAME = 'Siraj Ali';
