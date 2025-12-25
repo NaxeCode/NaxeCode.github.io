f35ac60690ec9ea0ecc9802ee5daf54e1371398b
“NaxeCode” <15963333+NaxeCode@users.noreply.github.com>
2025-12-25 23:11:03 +0100

    Hardcode Google Analytics ID and remove from workflow

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,11 +1,5 @@
 import { GoogleAnalytics } from '@next/third-parties/google';
 
 export function Analytics() {
-  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
-
-  if (!GA_TRACKING_ID) {
-    return null;
-  }
-
-  return <GoogleAnalytics gaId={GA_TRACKING_ID} />;
+  return <GoogleAnalytics gaId="G-BPXEKM7YBC" />;
 }
